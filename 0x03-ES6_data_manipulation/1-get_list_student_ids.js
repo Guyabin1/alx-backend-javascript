0b85b4a1d93c977023a08fbbdd496ae727047d73
Guyabin1 <martinnyagaya60@gmail.com>
2024-08-12 07:31:52 +0000

    Filter

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,16 @@
+/**
+ * Retrieves ids from a list of students.
+ * @param {{
+ *   id: Number,
+ *   firstName: String,
+ *   location: String
+ * }[]} students - The list of students.
+ * @author Maitin Nyagaya <https://github.com/Guyabin1>
+ * @returns
+ */
+export default function getListStudentIds(students) {
+  if (students instanceof Array) {
+    return students.map((student) => student.id);
+  }
+  return [];
+}
