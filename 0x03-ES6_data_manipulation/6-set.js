94b0a2d18cee1f864c7271d80137b25665e1a874
Guyabin1 <martinnyagaya60@gmail.com>
2024-08-12 13:15:47 +0000

    Set data structure

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author Martin Nyagaya <https://github.com/Guyabin1>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
